74ef34754d2d155a9e7fde819a406ad91c94ddea
Diablox777 <tetyakov2@gmail.com>
2024-02-15 18:27:32 +0700

    Оформил Error404, добавил SVG, прописал функционал рандомного позиционирования. Необходимо: выставить координаты + допилить стили для текста + разобраться, почему SVG не выводятся

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import SettingsPage from "./pages/SettingsPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => {
-
   return (
-    <div className="wrapper">
+    /* 
+    <div className="wrapper" >
       <SettingsPage />
+    </div> 
+    */
+    <div>
+      <ErrorPage />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/error-page/Error.js
+++ src/components/error-page/Error.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./error.css";
 
 const Error = () => {
-    return (
-        <div className="error-page">
-        <h1 className="error-code">404</h1>
-        <p className="error-message">Упс... страница не найдена</p>
-        </div>
-    );
-  };
-  
-  export default Error;
+  const initialPositions = [
+    { x: 50, y: 50 }, { x: 150, y: 50 }, { x: 250, y: 50 }, { x: 350, y: 50 },
+    { x: 50, y: 150 }, { x: 150, y: 150 }, { x: 250, y: 150 }, { x: 350, y: 150 },
+    { x: 50, y: 250 }, { x: 150, y: 250 }, { x: 250, y: 250 }, { x: 350, y: 250 },
+    { x: 50, y: 350 }, { x: 150, y: 350 }, { x: 250, y: 350 }, { x: 350, y: 350 },
+  ];
+
+  const svgNames = [
+    '#ampersand',
+    '#hashtag',
+    '#tilde',
+    '#triangle',
+    '#underscore'
+  ];
+
+  const [positions, setPositions] = useState([]);
+
+  useEffect(() => {
+    const shuffledPositions = initialPositions.map((position, index) => ({
+      x: position.x,
+      y: position.y,
+      name: svgNames[index % svgNames.length],
+      rotation: Math.random() * 360 
+    }));
+
+    setPositions(shuffledPositions);
+  }, []);
+
+  return (
+    <div className="error-page">
+      <h1 className="error-code">404</h1>
+      <p className="error-message">Упс... страница не найдена</p>
+      <div>
+        {positions.map((position, index) => (
+          <svg
+            key={index}
+            style={{
+              position: 'absolute',
+              left: `${position.x}px`,
+              top: `${position.y}px`,
+              transform: `rotate(${position.rotation}deg)` // Apply random rotation
+            }}
+            width="30"
+            height="30"
+          >
+            <use xlinkHref={position.name} />
+          </svg>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Error;
